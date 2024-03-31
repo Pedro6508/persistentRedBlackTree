d0fc2eb0a1189c2b558e657c518788dfaea3bf55
Pedro Vinicius Coelho belem <pevine6508@gmail.com>
2024-03-31 10:05:12 -0300

    add test spec

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,24 @@
+import Test.Hspec
+import Tree
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  describe "Tree" $ do
+   it "inserts a value" $ do
+     let tree = insert 5 Empty
+     contains 5 tree `shouldBe` True
+
+   it "deletes a value" $ do
+     let tree = insert 5 Empty
+     let newTree = delete 5 tree
+     case newTree of
+       Just t -> contains 5 t `shouldBe` False
+       Nothing -> True `shouldBe` True
+
+   it "searches for a value" $ do
+     let tree = insert 5 Empty
+     search 5 tree `shouldBe` Just 5
+
+   it "performs an inorder traversal" $ do
+     let tree = foldr insert Empty [5, 3, 7, 1, 4, 6, 8]
+     inorder tree `shouldBe` [1, 3, 4, 5, 6, 7, 8]
