75977019e6859c0fb007161b02389f8eb245803c
Pedro Vinicius Coelho belem <pevine6508@gmail.com>
2024-04-28 17:10:52 -0300

    feat: implement insert to leaf

--- src/RedBlack.hs
+++ src/RedBlack.hs
@@ -19,7 +19,9 @@ newtype RedBlack a = RedBlack (Tree (a, Color)) deriving (Show, Eq)
 instance BasicOps RedBlack where
   insert x (RedBlack t)
     | t == Empty = RedBlack (Node (x, Black) Empty Empty)
-    | otherwise = undefined
+    | t@(Node (a, Black) Empty Empty) <- t = if x < a
+      then RedBlack (Node (a, Black) (Node (x, Red) Empty Empty) Empty)
+      else RedBlack (Node (a, Black) Empty (Node (x, Red) Empty Empty))
   delete x (RedBlack t) = undefined
   search x (RedBlack t) = undefined
   inorder (RedBlack t) = undefined
--- test/RedBlackSpec.hs
+++ test/RedBlackSpec.hs
@@ -14,3 +14,7 @@ rbSpec = describe "RedBlack" $ do
     let tree = RedBlack Empty
     let tree' = insert 1 tree
     tree' `shouldBe` RedBlack (Node (1, Black) Empty Empty)
+  it "adds a new node to a tree with a single black node" $ do
+    let tree = RedBlack (Node (1, Black) Empty Empty)
+    let tree' = insert 2 tree
+    tree' `shouldBe` RedBlack (Node (1, Black) Empty (Node (2, Red) Empty Empty))
