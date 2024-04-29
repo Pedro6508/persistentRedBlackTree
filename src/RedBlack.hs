1223d965491a636daf9f7e922f90a91a44fb3319
Pedro Vinicius Coelho belem <pevine6508@gmail.com>
2024-04-28 21:38:08 -0300

    feat: balance full implementation

--- src/RedBlack.hs
+++ src/RedBlack.hs
@@ -15,25 +15,37 @@ class RedBlackOps t where
 data Color = B
   | R deriving (Show, Read, Eq)
 
+instance Ord Color where
+  compare B R = GT
+  compare R B = LT
+  compare _ _ = EQ
+
 newtype RedBlack a = RedBlack (Tree (a, Color)) deriving (Show, Eq)
 leaf :: a -> Color -> Tree (a, Color)
 leaf x c = N (x, c) E E
 
-balance :: Tree (a, Color) -> RedBlack a
-balance (N (z, B) (N (y, R) (N (x, R) a b) c) d)
-	= RedBlack (N (y, R) left right)
-	where
-		left = N (x, B) a b
-		right = N (z, B) c d
-balance _ = undefined
+fmt (x, y, z) (a, b, c, d) = RedBlack (N (y, R) left right)
+    where
+      left = N (x, B) a b
+      right = N (z, B) c d
+
+balance :: RedBlack a -> RedBlack a
+balance (RedBlack t)
+	| t@(N (z, B) (N (y, R) (N (x, R) a b) c) d) <- t = fmt (x, y, z) (a, b, c, d)
+	| t@(N (z, B) (N (x, R) a (N (y, R) b c)) d) <- t = fmt (x, y, z) (a, b, c, d)
+	| t@(N (x, B) a (N (z, R) (N (y, R) b c) d)) <- t = fmt (x, y, z) (a, b, c, d)
+	| t@(N (x, B) a (N (z, R) b (N (y, R) c d))) <- t = fmt (x, y, z) (a, b, c, d)
+	| otherwise = RedBlack t
 
 instance BasicOps RedBlack where
   insert x (RedBlack t)
     | t == E = RedBlack (N (x, B) E E)
-    | t@(N (a, B) E E) <- t = if x < a
-      then RedBlack (N (a, B) x' E)
-      else RedBlack (N (a, B) E x')
-      where x' = leaf x R
+    | t@(N (a, R) l r) <- t = if x < a
+      then balance (RedBlack (N (a, R) (insert (x, R) l) r))
+      else balance (RedBlack (N (a, R) l (insert (x, R) r)))
+    | t@(N (a, B) l r) <- t = if x < a
+      then (RedBlack (N (a, B) (insert (x, R) l) r))
+      else (RedBlack (N (a, B) l (insert (x, R) r)))
   delete x (RedBlack t) = undefined
   search x (RedBlack t) = undefined
   inorder (RedBlack t) = undefined
--- test/RedBlackSpec.hs
+++ test/RedBlackSpec.hs
@@ -8,6 +8,14 @@ import RedBlack (RedBlack(..), Color(..), balance)
 import Tree (Tree(..))
 import BasicOps (insert)
 
+populateRedBlack :: [Int] -> RedBlack Int
+populateRedBlack = foldr insert (RedBlack E)
+
+findDoubleRed :: RedBlack a -> Bool
+findDoubleRed (RedBlack E) = False
+findDoubleRed (RedBlack (N (_, R) (N (_, R) _ _) _)) = True
+findDoubleRed _ = False
+
 rbSpec :: SpecWith ()
 rbSpec = describe "RedBlack" $ do
   it "adds a new node to an empty tree" $ do
@@ -19,9 +27,6 @@ rbSpec = describe "RedBlack" $ do
     let tree' = insert 2 tree
     tree' `shouldBe` RedBlack (N (1, B) E (N (2, R) E E))
   it "balance a tree with a red parent and son" $ do
-		let tree = N (3, B) (N (2, R) (N (1, R) E E) E) E
-		let tree' = balance tree
-		tree' `shouldBe` RedBlack (N (2, R) a b)
-			where
-				a = N (1, B) E E
-				b = N (3, B) E E
+		let tree = populateRedBlack [3, 4, 5, 1, 2, 5, 6, 7, 9, 10]
+		let hasDoubleRed' = (findDoubleRed . balance) tree
+		hasDoubleRed' `shouldBe` False
